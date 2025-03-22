cae1af0c6545478c640fd1008aa71fabce4769ec
Fernando Del Lama <dellama.f@gmail.com>
2025-03-22 03:26:33 -0300

    Modelagem em inglês

--- brunetto_pi/brunetto/models.py
+++ brunetto_pi/brunetto/models.py
@@ -3,28 +3,30 @@ from django.utils import timezone
 
 
 class BaseModel(models.Model):
-    criado_em = models.DateTimeField(default=timezone.now)
-    atualizado_em = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
 
 
-class Cargo(BaseModel):
-    nome = models.CharField(max_length=100)
+class Position(BaseModel):
+    name = models.CharField(max_length=100)
 
 
-class Colaborador(BaseModel):
-    nome = models.CharField(max_length=100)
-    matricula = models.IntegerField(unique=True)
-    cargo = models.ForeignKey(Cargo, on_delete=models.CASCADE, related_name='colaboradores')
+class Employee(BaseModel):
+    name = models.CharField(max_length=100)
+    registration_number = models.IntegerField(unique=True)
+    position = models.ForeignKey(Position, on_delete=models.CASCADE, related_name='employees')
 
 
-class Massa(BaseModel):
-    nome = models.CharField(max_length=100)
+class Pasta(BaseModel):
+    name = models.CharField(max_length=100)
 
-class RegistroDiarioProducao(BaseModel):
-    autor = models.ForeignKey(Colaborador, on_delete=models.CASCADE, related_name='autor_registros')
-    lider_producao = models.ForeignKey(Colaborador, on_delete=models.CASCADE, related_name='lider_registros')
-    data = models.DateField()
-    produto_acabado_massas = models.ForeignKey(Massa, on_delete=models.CASCADE, related_name='massa_acabadas')
+class ProductionDailyRecord(BaseModel):
+    author = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='authored_records')
+    production_leader = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='led_records')
+    date = models.DateField()
+    finished_pasta = models.ForeignKey(Pasta, on_delete=models.CASCADE, related_name='massa_acabadas')
+    quantity = models.IntegerField()
+    waste = models.FloatField()
